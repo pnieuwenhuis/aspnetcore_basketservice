782f721ebaacdfd8ef7d74598eb009dff1d20313
Paul Nieuwenhuis <Paul Nieuwenhuis>
2016-10-18 17:04:07 +0200

    Some spacing

--- src/BasketService/Products/Routes/GetAllProducts.cs
+++ src/BasketService/Products/Routes/GetAllProducts.cs
@@ -9,11 +9,13 @@ namespace BasketService.Products.Routes
     {
         private ILogger<GetAllProducts> Logger { get; set; }
         private IActorRef ProductsActor { get; set; }
+
         public GetAllProducts(ProductsActorProvider provider, ILogger<GetAllProducts> logger)
         {
             this.Logger = logger;
             this.ProductsActor = provider.Get();
         }
+
         public async Task<IEnumerable<Product>> Execute() {
             Logger.LogInformation("Requesting all products");
             return await this.ProductsActor.Ask<IEnumerable<Product>>(
